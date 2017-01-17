a12985e908de3fbea6ae2fea35cf1c9f99991f23
Joshua Zenn <jzenn@students.kennesaw.edu>
2017-01-17 17:36:40 +0000

    Fixed authentication issues in CI environments.

--- frontend/main.rb
+++ frontend/main.rb
@@ -20,19 +20,27 @@ enable :sessions
 
 helpers do
   def login?
+    return true if is_ci?
     return (!session[:auth].nil? and (session[:auth] == true))
   end
 
   def login_admin?
+    return true if is_ci?
     return (!session[:auth_admin].nil? and (session[:auth_admin] == true))
   end
 
   def login_super?
-    return ((!session[:auth_super].nil? and (session[:auth_super] == true)) or (ENV['CI'] == 'true'))
+    return true if is_ci?
+    return (!session[:auth_super].nil? and (session[:auth_super] == true))
   end
   
   def is_logged_in?
+    return true if is_ci?
     return (login? or login_admin? or login_super?)
+  end
+  
+  def is_ci?
+    return (ENV['CI'] == 'true')
   end
   
   def login_username
