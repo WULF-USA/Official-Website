f7112528e7e74e27754ff16a05b7e1da5ec953d9
Joshua Zenn <jzenn@students.kennesaw.edu>
2017-02-01 17:41:08 +0000

    Fixed testing errors.

--- spec/cva_spec.rb
+++ spec/cva_spec.rb
@@ -2,9 +2,8 @@ require_relative './spec_helper'
 
 describe WulfApp do
     it "CVA-001: CSRF in SSO authentication" do
-        Capybara.using_session("CSRF sandbox") do
-            visit "/sso/author/login"
-        end
+        visit "/sso/author/login"
+        page.driver.clear_cookies
         fill_in 'Username', with: 'testuser'
         fill_in 'Password', with: 'testpassword'
         click_on 'Sign in'
