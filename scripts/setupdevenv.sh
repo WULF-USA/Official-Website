acd4948f9758e3fdc7207f1aef7fb1aece47faf4
Joshua Zenn <jzenn@students.kennesaw.edu>
2017-02-09 03:08:57 +0000

    Fixed dev setup script.

--- scripts/setupdevenv.sh
+++ scripts/setupdevenv.sh
@@ -2,6 +2,7 @@
 echo 'Setting up environment'
 sudo service postgresql restart
 sudo service redis-server restart
+memcached -d
 export REDIS_URL=redis://127.0.0.1:6379
 export APP_SUPER_UNAME=testuser
 export APP_SUPER_PASSWD=testpassword
