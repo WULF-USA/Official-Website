2843b73cfe0ce57448f9428c873ddd88bf57c6d1
Joshua Zenn <jzenn@students.kennesaw.edu>
2017-02-09 17:01:01 +0000

    Added service dependency for Memcached.

--- lib/cache.rb
+++ lib/cache.rb
@@ -4,7 +4,7 @@ require_relative '../jobs/reload_cache'
 
 module Lib
     module Cache
-        DC = Dalli::Client.new('localhost:11211', { :namespace => "wulfusa", :compress => true })
+        DC = Dalli::Client.new(ENV['MEMCACHIER_SERVERS'], { :namespace => "wulfusa", :compress => true })
         def Cache.read(key)
             return DC.get(key)
         end
