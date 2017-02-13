2609daf7f8cdee617635cf40f6d8f73c011b7021
Joshua Zenn <jzenn@students.kennesaw.edu>
2017-02-13 21:18:39 +0000

    Removed ES6-only JS code.

--- public/src/js/jobtracking.js
+++ public/src/js/jobtracking.js
@@ -2,26 +2,25 @@ $(document).on('click', '.notifyjs-bootstrap-base', function() {
     location.reload(true);
 });
 
-var taskID = setInterval(function(){
+var taskID = setInterval(function() {
     var div = document.getElementById('msgHolder'),
-    divChildren = div.childNodes;
-    if(divChildren.length === 0) {
+        divChildren = div.childNodes;
+    if (divChildren.length === 0) {
         clearInterval(taskID);
     }
-    for (let i=0; i<divChildren.length; i++) {
+    for (var i = 0; i < divChildren.length; i++) {
         $.get(
-            "/api/v1/job/" + divChildren[i].getAttribute('value'),
-            {},
+            "/api/v1/job/" + divChildren[i].getAttribute('value'), {},
             function(data) {
                 var obj = JSON.parse(data);
-                if(obj.status === "completed") {
+                if (obj.status === "completed") {
                     $.notify(obj.message + " Click to refresh.", {
                         className: "success",
                         autoHide: false
                     });
                     clearInterval(taskID);
                 }
-                if(obj.status === "failed") {
+                if (obj.status === "failed") {
                     $.notify(obj.msg, {
                         className: "error",
                         autoHide: true
